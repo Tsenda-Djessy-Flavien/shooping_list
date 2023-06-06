d6836e19cbdd3f9197883600355c76f7e37d6f1c
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-06 14:02:22 +0100

    Getting Form Access via a Global Key

--- lib/presentation/screens/new_item_screen.dart
+++ lib/presentation/screens/new_item_screen.dart
@@ -9,6 +9,14 @@ class NewItemScreen extends StatefulWidget {
 }
 
 class _NewItemScreenState extends State<NewItemScreen> {
+  // verification supplementaire des types
+  // et des suggestions d'autocompletion
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +26,8 @@ class _NewItemScreenState extends State<NewItemScreen> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          // cette key nous permet de savoir s'il faut afficher les erreurs de validations du formulaire ou pas
+          key: _formKey,
           child: Column(
             children: [
               // TextFormField -> instead of TextField()
@@ -87,11 +97,13 @@ class _NewItemScreenState extends State<NewItemScreen> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: _saveItem,
                     child: const Text('Add Item'),
                   ),
                 ],
