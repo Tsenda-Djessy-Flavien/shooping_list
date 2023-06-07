f98921d662a4fda08a3a402ab9be3cc7c80e2184
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-07 09:26:25 +0100

    removeItem to list

--- lib/presentation/screens/grocery_list_screen.dart
+++ lib/presentation/screens/grocery_list_screen.dart
@@ -28,21 +28,62 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
     });
   }
 
+  void _onRemoveItem(GroceryModel groceryItem) {
+    final checkGroceryItem = _groceryItems.contains(groceryItem);
+    if (checkGroceryItem) {
+      setState(() {
+        _groceryItems.remove(groceryItem);
+      });
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('delete'),
+          action: SnackBarAction(
+            label: 'undo',
+            onPressed: () {
+              setState(() {
+                _groceryItems.add(groceryItem);
+              });
+            },
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget buildMain = const Center(
+      child: Text('No items added yet.'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      buildMain = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+          key: ValueKey(_groceryItems[index].id),
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+            margin: Theme.of(context).cardTheme.margin,
+          ),
+          child: GroceryItem(
+            color: _groceryItems[index].category.color,
+            title: _groceryItems[index].name,
+            quantity: '${_groceryItems[index].quantity}',
+          ),
+          onDismissed: (direction) {
+            _onRemoveItem(_groceryItems[index]);
+          },
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => GroceryItem(
-          color: _groceryItems[index].category.color,
-          title: _groceryItems[index].name,
-          quantity: '${_groceryItems[index].quantity}',
-        ),
-      ),
+      body: buildMain,
     );
   }
 }
